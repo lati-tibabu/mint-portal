d73b69fe1b69ee42e9e6649505a23638857b889c
Lati Tibabu <84601274+lati-tibabu@users.noreply.github.com>
2023-07-28 22:02:50 +0300

    July 28 update

--- js/behavior.js
+++ js/behavior.js
@@ -1,12 +1,15 @@
 window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
   const logo = document.getElementById("logo");
+  const co_name = document.getElementById("co_name")
   if (window.scrollY > 0) {
     header.classList.add("shrink");
     logo.classList.add("logoReduce");
+    co_name.classList.add("mint_name_shrink");
   } else {
     header.classList.remove("shrink");
     logo.classList.remove("logoReduce");
+    co_name.classList.add("mint_name_shrink");
   }
 });
 

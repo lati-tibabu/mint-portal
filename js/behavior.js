6a93f52319ba3b234c4df5aa76fa6d66474a3c43
Lati Tibabu <84601274+lati-tibabu@users.noreply.github.com>
2023-08-04 09:50:42 +0300

    August 2, 2023 Updates

--- js/behavior.js
+++ js/behavior.js
@@ -2,14 +2,19 @@ window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
   const logo = document.getElementById("logo");
   const co_name = document.getElementById("co_name")
+  const more_nav_bars = document.getElementById("more-nav-links");
+
   if (window.scrollY > 0) {
     header.classList.add("shrink");
     logo.classList.add("logoReduce");
     co_name.classList.add("mint_name_shrink");
+    more_nav_bars.classList.add("more-nav-bars-reduced")
+    more_nav_bars.style.display="none";
   } else {
     header.classList.remove("shrink");
     logo.classList.remove("logoReduce");
-    co_name.classList.add("mint_name_shrink");
+    co_name.classList.remove("mint_name_shrink");
+    more_nav_bars.classList.remove("more-nav-bars-reduced")
   }
 });
 
--- js/header.js
+++ js/header.js
@@ -6,3 +6,22 @@ document.addEventListener("DOMContentLoaded", function () {
     .then((content) => (headerPlaceholder.innerHTML = content))
     .catch((error) => console.error("Error: ", error));
 });
+
+// const more_nav = document.getElementById("more-nav-links");
+let more_expanded = 0;
+
+function more_nav_links() {
+  const more_nav_bars = document.getElementById("more-nav-links");
+
+  if (more_expanded === 0) {
+    more_nav_bars.style.display = "flex";
+    more_nav_bars.style.height = "350px";
+    more_expanded = 1;
+  } else {
+    more_nav_bars.style.display = "none";
+    more_nav_bars.style.height = "0";
+    more_expanded = 0;
+  }
+
+  // alert("hello there");
+}
